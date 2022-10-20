19c95c8163de8521b5a03b7881914ca51d9ecf2a
kalkulator413 <kalkin413@gmail.com>
2022-10-19 23:57:53 -0700

    finish some unit tests

--- DocSearchServerTest.java
+++ DocSearchServerTest.java
@@ -2,14 +2,28 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.net.URI;
 import java.nio.file.Path;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class DocSearchServerTest {
 
     @Test
-    public static void unitTest() {
-        Handler handler = new Handler("./");
-        // int numFiles = FileHelpers.getFiles(new Path("/")).size();
-        // assertEquals(handler.handleRequest(new URI('/'), "There are " + numFiles + " files to search."));
+    public void unitTest() throws IOException, URISyntaxException{
+        Handler h = new Handler("./");
+        String s1 = h.handleRequest(new URI(""));
+        assertEquals(s1, "404 Not Found!");
 
+        String s2 = h.handleRequest(new URI("/"));
+        List<File> files = FileHelpers.getFiles(Paths.get("."));
+        int size = 0;
+        for (File f : files) {
+            if (!f.isDirectory()) {
+                size += 1;
+            }
+        }
+        assertEquals(s2, "There are " + size + " files to search.");
     }
 }
--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 set -e
 
 javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore DocSearchServerTests
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore DocSearchServerTest
